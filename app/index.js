c8b03ca260c46c167ac73ae00689ebc820e2f8cb
Ken Huh <ken123777@gmail.com>
2019-04-19 01:00:29 +0900

    Update README

--- app/index.js
+++ app/index.js
@@ -1,12 +1,6 @@
 // Load application styles
 import 'styles/index.css';
-import server from './server';
 
 // ================================
 // START YOUR APP HERE
 // ================================
-
-// You can get the initial data
-server.get(function (data) {
-  alert('Here is the data: ' + JSON.stringify(data));
-});
